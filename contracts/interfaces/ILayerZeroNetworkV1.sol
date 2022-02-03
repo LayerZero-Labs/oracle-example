e8357a238b82e5b8f7e7b1aad497036719dc358b
caleb <caleb.banister@gmail.com>
2022-02-03 15:36:39 -0500

    test

---
+++ contracts/interfaces/ILayerZeroNetworkV1.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity >=0.7.0;
+
+interface ILayerZeroNetworkV1 {
+    function updateBlockHeader(uint16 _srcChainId, address _oracle, bytes calldata _blockHash, uint _confirmations, bytes calldata _data ) external;
+}
+
+
