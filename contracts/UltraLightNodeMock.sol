915a386252b7316d2754d9094ab3270f6a69c3d9
caleb <caleb.banister@gmail.com>
2022-02-10 10:53:52 -0500

    add ability for Oracle to withdraw accumulated fees

--- contracts/UltraLightNodeMock.sol
+++ contracts/UltraLightNodeMock.sol
@@ -2,10 +2,15 @@
 
 pragma solidity ^0.8.11;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 import "./interfaces/ILayerZeroUltraLightNodeV1.sol";
 
 // this is a mocked LayerZero UltraLightNodeMock that receives the blockHash and receiptsRoot
-contract UltraLightNodeMock is ILayerZeroUltraLightNodeV1 {
+contract UltraLightNodeMock is ILayerZeroUltraLightNodeV1, ReentrancyGuard {
+
+    // oracle fees will accumulate in the LayerZero contract
+    mapping(address => uint) public oracleQuotedFees;
 
     struct BlockData {
         uint          confirmations;
@@ -13,6 +18,7 @@ contract UltraLightNodeMock is ILayerZeroUltraLightNodeV1 {
     }
 
     event HeaderReceived(uint16 srcChainId, address oracle, uint confirmations, bytes blockhash);
+    event WithdrawNative(address _owner, address _to, uint _amount);
 
     mapping(address => mapping(uint16 => mapping(bytes => BlockData))) public blockHeaderLookup;
 
@@ -33,7 +39,16 @@ contract UltraLightNodeMock is ILayerZeroUltraLightNodeV1 {
         emit HeaderReceived(_srcChainId, msg.sender, _confirmations, _blockHash );
     }
 
-    function withdrawOracleFee(address _to, uint _amount) external override {
-        // todo - let the msg.sender oralce withdraw their accumulated fees
+    function withdrawOracleFee(address _to, uint _amount) override nonReentrant external {
+        oracleQuotedFees[msg.sender] = oracleQuotedFees[msg.sender] - _amount;
+        _withdrawNative(msg.sender, _to, _amount);
+    }
+
+    //---------------------------------------------------------------------------
+    // Claim Fees
+    function _withdrawNative(address _from, address _to, uint _amount) internal {
+        (bool success, ) = _to.call{value: _amount}('');
+        require(success, "LayerZero: withdraw failed");
+        emit WithdrawNative(_from, _to, _amount);
     }
 }
