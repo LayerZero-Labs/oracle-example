2823ae8b870b84f01175ce0bf2e5bf89356ea81d
caleb <caleb.banister@gmail.com>
2022-02-22 09:03:59 -0500

    text

--- contracts/interfaces/ILayerZeroOracle.sol
+++ contracts/interfaces/ILayerZeroOracle.sol
@@ -9,7 +9,13 @@ interface ILayerZeroOracle {
     function getPrice(uint16 dstChainId) view external returns (uint priceInWei);
 
     // initiates the oracle to do its job
-    function notifyOracle(uint16 _dstChainId, uint16 _outboundProofType, bytes32 _remoteUlnAddress, uint64 _outboundBlockConfirmations, bytes32 _payloadHash) external;
+    function notifyOracle(
+        uint16 _dstChainId,                         // LayerZero destination endpoint id
+        uint16 _outboundProofType,                  // Default: 1, specifies the type of data being moved.
+        bytes32 _remoteUlnAddress,                  // LayerZero destination contract address for updateHash()
+        uint64 _outboundBlockConfirmations,         // block confirmations to wait
+        bytes32 _payloadHash                        // hash of the payload data, from source notifyOracle() call
+    ) external;
 
     // return true if the address is allowed to call updateBlockHeader()
     function isApproved(address oracleSigner) view external returns (bool approved);
